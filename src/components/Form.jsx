2c29d715aa0f0c0e67427a07538cccf53ac8171b
rutuja13 <thatterutuja2195@gmail.com>
2022-04-19 01:18:49 -0400

    build update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,27 +1,8 @@
-import { makeStyles } from "@material-ui/core";
 import React from "react";
 import { useEffect, useState } from "react";
 import {getData} from "../service/api"
 import Information from "./Information"
 
-const useStyles = makeStyles({
-    input:{
-        color : '#fff' , 
-        marginRight : 15 , 
-
-    } , 
-    component :{
-        padding : 10 , 
-        background : '#445a6f'
-    },
-    button :{
-        width : 150,
-        height : 40,
-        background : '#e67e22',
-        color : '#fff',
-        marginTop :5
-    }
-})
 
 const Form = () =>{
     const [data , getWeatherData ] = useState();
